4e0be366de3a6aaead12855680ea3e179eb2e781
soni kumari <sonikumari34624@gmail.com>
2024-07-07 15:02:20 +0530

    finall commit

--- src/components/postForm/postForm.jsx
+++ src/components/postForm/postForm.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Input, RTE, Select } from "./index";
+import { Button, Input, RTE, Select } from "..";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
